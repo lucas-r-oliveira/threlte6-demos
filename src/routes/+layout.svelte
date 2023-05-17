b98066357fa93c2c3d6ba683db712158d9c7d48a
lucas-r-oliveira <lucasoliveira7c@gmail.com>
2023-05-17 22:19:17 +0100

    initial setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<div class="scene">
+	<slot />
+</div>
+
+<style>
+	.scene {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		inset: 0;
+		background-attachment: fixed;
+		background: radial-gradient(#e66465, #9198e5);
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Canvas, T } from '@threlte/core'
+</script>
+
+<Canvas>
+	<T.Mesh>
+		<T.BoxGeometry attach="geometry" />
+		<T.MeshBasicMaterial attach="material" />
+	</T.Mesh>
+</Canvas>
+
